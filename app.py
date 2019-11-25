f4f5bdbfcbfa133516e46feae047d9a05fe9b2c1
Angelo Steffenel <lsteffenel@gmail.com>
2019-11-25 15:32:34 +0100

    Update app.py
    put the "html" variable at the right level

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ def hello():
         visits = redis.incr("counter") 
     except RedisError: 
         visits = "<i>cannot connect to Redis, counter disabled</i>" 
-        html = "<h3>Hello {name}!</h3>" \
-        "<b>Hostname:</b> {hostname}<br/>" \
-        "<b>Visits:</b> {visits}" 
+    html = "<h3>Hello {name}!</h3>" \
+    "<b>Hostname:</b> {hostname}<br/>" \
+    "<b>Visits:</b> {visits}" 
     return html.format(name=os.getenv("NAME", "world"), \
     hostname=socket.gethostname(), visits=visits) 
     
